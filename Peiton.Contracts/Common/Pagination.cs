207f5258e5adf4477f455fb76934ef30ca4ee7aa
Gonzalo Corchón <gonzalo.corchon@prodigious.com>
2024-11-01 10:31:27 +0100

    Hacer parámetros de paginación obligatorios

--- Peiton.Contracts/Common/Pagination.cs
+++ Peiton.Contracts/Common/Pagination.cs
@@ -1,12 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Peiton.Contracts.Common;
 
 public class Pagination
 {
+    [BindRequired()]
     public int Page { get; set; }
 
+    [BindRequired()]
     public int Total { get; set; }
 }
 
